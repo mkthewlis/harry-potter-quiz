b5a6f3db0f61988d7fcb6e6cbd36568435c1a150
mkthewlis <miranda.thewlis@jayway.com>
2020-11-22 15:51:53 +0100

    feat: added functionality to store result and display it to user

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -7,10 +7,14 @@ import './Quiz.css';
 export const Quiz = () => {
     const tenRandomQuestions = randomiseQuizQuestions(quizQuestions);
     const [questionNumber, setQuestionNumber] = useState(0);
+    const [result, setResult] = useState(0);
 
-    const handleAnswerClick = () => {
+    const handleAnswerClick = (correctAnswer) => {
+        if (correctAnswer === true && questionNumber < 9){
+            setResult(result + 1);
+        };
         const continueQuiz = questionNumber + 1;
-        if (continueQuiz < 10) {
+        if (continueQuiz < 10){
             setQuestionNumber(continueQuiz);
         };
     };
@@ -22,10 +26,14 @@ export const Quiz = () => {
         </header>
         <main>
             <h2 className="quiz__question-number">Question {questionNumber + 1}/{tenRandomQuestions.length}</h2>
+            <div className="quiz__statistics-container">
+                <div className="quiz__statistics-box">Your score: {result}</div>
+                <div className="quiz__statistics-box">Time remaining: 15.00</div>
+            </div>
             <p className="quiz__question">{tenRandomQuestions[questionNumber].question}</p>
             <div className="quiz__answer-container">
-                {tenRandomQuestions[questionNumber].possibleAnswers.map((possibleAnswers, index) => (
-                    <Button onClick={handleAnswerClick} answer={possibleAnswers.answer}/>
+                {tenRandomQuestions[questionNumber].possibleAnswers.map((possibleAnswer, index) => (
+                    <Button onClick={() => handleAnswerClick(possibleAnswer.correctAnswer)} answer={possibleAnswer.answer}/>
                 ))}
             </div>
         </main>
