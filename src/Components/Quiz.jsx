c9cf1bc6e0979d6bd8125a8158ee606faa0d768a
mkthewlis <miranda.thewlis@jayway.com>
2020-11-22 16:37:26 +0100

    feat: added Results component to render statistics after quiz completion

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { quizQuestions } from '../QuizData/QuizQuestions';
 import { randomiseQuizQuestions } from '../Calculations/RandomiseQuizQuestions';
 import { Button } from './QuizComponents/Button';
+import { Result } from './QuizComponents/Results';
 import './Quiz.css';
 
 export const Quiz = () => {
     const tenRandomQuestions = randomiseQuizQuestions(quizQuestions);
     const [questionNumber, setQuestionNumber] = useState(0);
     const [result, setResult] = useState(0);
+    const [viewResult, setViewResult] = useState(false);
 
     const handleAnswerClick = (correctAnswer) => {
         if (correctAnswer === true && questionNumber < 9){
@@ -17,25 +19,34 @@ export const Quiz = () => {
         if (continueQuiz < 10){
             setQuestionNumber(continueQuiz);
         };
+        if (questionNumber === 9){
+            setViewResult(true);
+        }
     };
 
     return(
     <div className="quiz__wrapper">
         <header>
-            <h1 className="quiz__header">Harry Potter Pub Quiz</h1>
+            <h1 className="quiz__header">{!viewResult ? ('Harry Potter Pub Quiz') : ('Congratulations!')}</h1>
         </header>
         <main>
-            <h2 className="quiz__question-number">Question {questionNumber + 1}/{tenRandomQuestions.length}</h2>
-            <div className="quiz__statistics-container">
-                <div className="quiz__statistics-box">Your score: {result}</div>
-                <div className="quiz__statistics-box">Time remaining: 15.00</div>
-            </div>
-            <p className="quiz__question">{tenRandomQuestions[questionNumber].question}</p>
-            <div className="quiz__answer-container">
-                {tenRandomQuestions[questionNumber].possibleAnswers.map((possibleAnswer, index) => (
-                    <Button onClick={() => handleAnswerClick(possibleAnswer.correctAnswer)} answer={possibleAnswer.answer}/>
-                ))}
-            </div>
+            {!viewResult ? (
+                <>
+                    <h2 className="quiz__question-number">Question {questionNumber + 1}/{tenRandomQuestions.length}</h2>
+                    <div className="quiz__statistics-container">
+                        <div className="quiz__statistics-box">Your score: {result}</div>
+                        <div className="quiz__statistics-box">15.00s</div>
+                    </div>
+                    <p className="quiz__question">{tenRandomQuestions[questionNumber].question}</p>
+                    <div className="quiz__answer-container">
+                        {tenRandomQuestions[questionNumber].possibleAnswers.map((possibleAnswer, index) => (
+                            <Button onClick={() => handleAnswerClick(possibleAnswer.correctAnswer)} answer={possibleAnswer.answer}/>
+                        ))}
+                    </div>
+                </>
+                ) : (
+                <Result result={result} tenRandomQuestions={tenRandomQuestions}/>
+            )}
         </main>
     </div>
     );
---
+++ src/Components/QuizComponents/Results.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Results.css';
+import './Button.css';
+
+export const Result = ({result, tenRandomQuestions}) => (
+    <> {result > 5 ? (
+        <h2 className="results__header">Hogwarts awaits you!</h2>
+    ) : (
+        <h2 className="results__header">Better luck next time!</h2>
+    )}
+        <ul className="results__statistics">
+            <li>Number of correct answers: {result} of {tenRandomQuestions.length}</li>
+            <li>Number of incorrect answers: {tenRandomQuestions.length - result} of {tenRandomQuestions.length}</li>
+        </ul>
+    </>
+);
